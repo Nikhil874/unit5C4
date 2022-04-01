afe2844a1928e4944b09f395aba0005c8a7ddfd7
Nikhil874 <95832751+Nikhil874@users.noreply.github.com>
2022-04-01 17:17:04 +0530

    final

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,6 +1,6 @@
 export const NotFound = () => {
   return <div className="notFound">
     {/* Show some 404 not found image or component here */}
-    Not Found
+  <img src="https://kbimages.dreamhosters.com/images/Site_Not_Found_Dreambot.fw.png" alt="" />
   </div>
 };
